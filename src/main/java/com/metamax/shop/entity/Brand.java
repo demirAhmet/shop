f8f79ed609cf318ca838dd2c59e7e7c40bec604e
Ahmet Demir <ahmet2715269@gmail.com>
2023-08-15 13:24:17 +0300

    brand düzenleme yapıldı

--- src/main/java/com/metamax/shop/entity/Brand.java
+++ src/main/java/com/metamax/shop/entity/Brand.java
@@ -3,11 +3,28 @@ package com.metamax.shop.entity;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name="brand")
+@Table(name = "brand")
 public class Brand {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-@Column(name = "name")
+    @Column(name = "name")
     private String name;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
---
+++ src/main/java/com/metamax/shop/repository/brand/BrandRepository.java
@@ -0,0 +1,4 @@
+package com.metamax.shop.repository.brand;
+
+public interface BrandRepository {
+}
